38b51304ca7dd2058e0015190b0194e6076c4733
Felix Wetell <felix.wetell@gmail.com>
2018-11-15 17:00:37 +0100

    Added database seeder and migrations files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id')->unique();
+            $table->increments('id');
             $table->string('name');
+            $table->string('idNumber')->unique();
             $table->boolean('verified');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
