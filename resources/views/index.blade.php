4e8cb6158b9a1ae1c3ad912651567726fcf8cd1d
Felix Wetell <felix.wetell@gmail.com>
2018-11-22 17:52:33 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,8 @@
 
         <div class="row justify-content-center">
             <div class="col-md-8">
-            {{Auth::user()->exams}}
             @foreach(Auth::user()->exams as $exam)
-                <p>$exam->name</p>
+                <h1>{{ $exam->name }}</h1>
             @endforeach
             </div>
         </div>
