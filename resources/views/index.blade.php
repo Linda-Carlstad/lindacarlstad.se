fb42c940ad8fb432c97420c002e9c7f88dfbb054
Felix Wetell <felix.wetell@gmail.com>
2018-11-22 17:46:49 +0100

    Random

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,13 @@
 @section('title', 'Hem')
 @extends('layouts.app')
-
 @section('content')
 
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="row text-center justify-content-center">
+            <div class="d-flex align-items-center">
+                <div class="text-center">
+                    <h1>Linda Carlstad</h1>
+                    <h4>Linjeförening för datavetare</h4>
+                </div>
             </div>
         </div>
 
