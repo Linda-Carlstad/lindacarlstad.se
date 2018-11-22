6c06127e45052a764a11deddc4b7e24fa6880f31
Felix Wetell <felix.wetell@gmail.com>
2018-11-22 17:54:59 +0100

    random

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,15 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
             @foreach(Auth::user()->exams as $exam)
-                <h1>{{ $exam->name }}</h1>
+                <div class="bg-dark p-3">
+                    <h1>{{ $exam->name }}</h1>
+                    <p>
+                        {{ $exam->document }}
+                        <br>
+                        {{ $exam->user->name  }}
+                    </p>
+                </div>
+                <br>
             @endforeach
             </div>
         </div>
