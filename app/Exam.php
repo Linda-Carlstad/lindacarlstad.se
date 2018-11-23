e7c5ea00b431d8382ca4139da6ae02d0d952835e
Felix Wetell <felix.wetell@gmail.com>
2018-11-23 10:06:10 +0100

    Includede user

--- app/Exam.php
+++ app/Exam.php
@@ -2,14 +2,14 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
-
     public function user(){
         return $this->belongsTo('App\User');
     }
-    
+
     protected $table = 'exams';
 }
