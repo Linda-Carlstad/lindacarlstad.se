0a9efcb27231c81a8292c6bbac59fa71be1874ab
Felix Wetell <felix.wetell@gmail.com>
2018-12-03 15:23:20 +0100

    Composer

---
+++ storage/framework/views/0c02f7d19033a01166d253fdd8c63f811bfe72fd.php
@@ -0,0 +1,12 @@
+<?php $__env->startSection('title', 'Föreningen'); ?>
+
+
+<?php $__env->startSection('content'); ?>
+
+<div class="">
+
+</div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
