a3ae934d083e9f5b7fb3b93684a590ec30d5034f
Felix Wetell <felix.wetell@gmail.com>
2019-01-18 12:24:53 +0100

    Updated HTML structure

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -11,7 +11,11 @@
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="name">
         <label for="name">Name:</label>
+        <br>
         <input type="text" name="name" value="" placeholder="Name">
+        <br>
+        <br>
+        <button type="submit" name="button">Send</button>
     </form>
     <hr>
     <form class="" action="/user/{{ Auth::user()->id }}" method="post">
@@ -19,10 +23,15 @@
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="email">
         <label for="email">Email:</label>
+        <br>
         <input type="text" name="email" value="" placeholder="email">
         <br>
         <label for="emailPassword">Current Password:</label>
+        <br>
         <input type="text" name="emailPassword" value="" placeholder="password">
+        <br>
+        <br>
+        <button type="submit" name="button">Send</button>
     </form>
     </form>
     </form>
@@ -32,13 +41,19 @@
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="password">
         <label for="oldPassword">Current Password:</label>
+        <br>
         <input type="text" name="oldPassword" value="" placeholder="password">
         <br>
         <label for="password">New Password:</label>
+        <br>
         <input type="text" name="password" value="" placeholder="password">
         <br>
         <label for="newPassword">New Password:</label>
+        <br>
         <input type="text" name="newPassword" value="" placeholder="password">
+        <br>
+        <br>
+        <button type="submit" name="button">Send</button>
     </form>
 
 @endsection
