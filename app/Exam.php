0450744614144bc9b4818d474faac2c04793b3db
Felix Wetell <felix.wetell@gmail.com>
2019-01-18 09:32:54 +0100

    Improved structure

--- app/Exam.php
+++ app/Exam.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
-    public function user(){
-        return $this->belongsTo('App\User');
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'exams';
+
+    public function user()
+    {
+        return $this->belongsTo( 'App\User' );
     }
 
-    protected $table = 'exams';
+
 }
