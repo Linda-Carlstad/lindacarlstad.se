6daef71582526b93b9cfd36d91b329babf074d91
Andreas Hortlund <hortlund.andreas@gmail.com>
2019-01-18 12:00:24 +0100

    Update app/User.php

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'idNumber', 'verified', 'email', 'password',
+        'name', 'id_number', 'verified', 'email', 'password',
     ];
 
     /**
