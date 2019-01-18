3b8fb51278eb558fdebbcae58d9641eea794d26b
Felix Wetell <felix.wetell@gmail.com>
2019-01-18 10:44:42 +0100

    Fixed route to profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -54,9 +54,9 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit( Request $request )
     {
-        //
+        return view( 'user.edit' );
     }
 
     /**
