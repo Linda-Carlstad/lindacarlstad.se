085cd7e460b32316fc17f48c24510919ece8770b
Mackeboi <36602678+Mackeboi@users.noreply.github.com>
2019-01-19 15:01:40 +0100

    Removed <h4> and updated <p>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,6 @@
                 <img src="{{ asset( '/img/logo.png' ) }}" alt="Linda Carlstads logga">
                 <br>
                 <h1>Linda Carlstad</h1>
-                <h4>Linjeförening för datavetare</h4>
             </div>
         </div>
     </div>
@@ -21,14 +20,11 @@
         <div class="col-md-8 p-4">
             <h3>Linda Carlstad</h3>
             <p>
-                LINDA är en studentförening under karlstad universitet.
-                LINDA bevakar ditt intresse som student, såväl att du har det bra på
-                universitet som på fritiden, utbildningen kommer inte alltid att vara rolig
-                eller lätt och då hoppas vi att vi kan vara till din hjälp med studierna om
-                det så krävs eller med att bara ta det lugnt och umgås.
-                Bland annat brukar många av oss gå till studentpuben Bunkern där man kan gå och
-                ta en öl/läsk/kopp kaffe eller äta god mat och spela spel och ta det lugnt mellan
-                varven på skolan. LINDA grundades 1984.
+                Linjeförening för datavetare eller kort sagt Linda är en studentförening på karlstads universitet.
+                Linda bevakar ditt intresse som student, du ska ha det bra på universitet och fritiden.
+                Det är inte alltid roligt eller lätt att studera men då finns vi till för att hjälpa dig.
+                Vi vill även se till att berika ditt studentliv på många olika sätt så som sittningar eller hänga på
+                studentpuben Bunkern. Linda grundades år 1984.
             </p>
         </div>
         <div class="col-md-4 p-4 align-self-center">
