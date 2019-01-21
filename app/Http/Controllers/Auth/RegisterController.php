7d5ee3f459f57af166516037f438900954524c89
Felix Wetell <felix.wetell@gmail.com>
2019-01-21 16:13:48 +0100

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'idNumber' => $data['idNumber'],
+            'id_number' => $data['idNumber'],
         ]);
     }
 }
