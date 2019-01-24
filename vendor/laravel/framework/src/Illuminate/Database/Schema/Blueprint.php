a59b2d47bb149c38dfbb65fa0b3263e107375be6
Felix Wetell <felix.wetell@gmail.com>
2019-01-24 17:34:07 +0100

    Updated comopser

--- vendor/laravel/framework/src/Illuminate/Database/Schema/Blueprint.php
+++ vendor/laravel/framework/src/Illuminate/Database/Schema/Blueprint.php
@@ -134,6 +134,8 @@ class Blueprint
      *
      * @param  \Illuminate\Database\Connection  $connection
      * @return void
+     *
+     * @throws \BadMethodCallException
      */
     protected function ensureCommandsAreValid(Connection $connection)
     {
@@ -1282,7 +1284,7 @@ class Blueprint
     public function removeColumn($name)
     {
         $this->columns = array_values(array_filter($this->columns, function ($c) use ($name) {
-            return $c['attributes']['name'] != $name;
+            return $c['name'] != $name;
         }));
 
         return $this;
