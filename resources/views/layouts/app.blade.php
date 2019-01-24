679c3f73297a869aa1a1a0b78eb3a60496a3aba0
Mackeboi <36602678+Mackeboi@users.noreply.github.com>
2019-01-24 22:46:55 +0100

    Added Copyright in footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,6 +143,9 @@
                     </div>
                 </div>
             </div>
+            <div class="footer-copyright">
+              <p>Copyright © 2019 · All Rights Reserved · Studentföreningen Linda Carlstad</p>
+            </div>
         </footer>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
