f6cbeca938d6185a9e9a6c8d670233f25bea62f3
Felix Wetell <felix.wetell@gmail.com>
2019-02-04 07:36:08 +0100

    Fixed bad code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace( '_', '-', app()->getLocale() ) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=0.9, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config( 'app.name', 'Linda Carlstad' ) }} - @yield('title')</title>
         <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -55,7 +55,7 @@
                         </li>
                         <li class="nav-item">
                             @if ( Route::has( 'register' ) )
-                            <a class="nav-link" href="{{ route( 'register' ) }}">{{ __( 'Registrera' ) }}</a>
+                                <a class="nav-link" href="{{ route( 'register' ) }}">{{ __( 'Registrera' ) }}</a>
                             @endif
                         </li>
                         @else
