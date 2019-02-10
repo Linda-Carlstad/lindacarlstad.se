b3e7f2d3815a9e892784a5a0ee90c1e0c90532f6
Felix Wetell <felix.wetell@gmail.com>
2019-02-10 14:11:56 +0100

    Changed button CSS

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -57,10 +57,10 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                      <button type="submit" class="btn btn-primary" class="btnContact">
+                                      <button type="submit" class="btn btn-red" class="btnContact">
                                           {{ __('Skicka') }}
                                       </button>
-                                      <button type="reset" class="btn btn-primary" class="btnContact">
+                                      <button type="reset" class="btn btn-red" class="btnContact">
                                           {{ __('Rensa') }}
                                       </button>
                           </div>
