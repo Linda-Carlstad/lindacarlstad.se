8491af6aa4bb8ecc4914f6e9e59491a932a6563b
Felix Wetell <felix.wetell@gmail.com>
2019-02-10 15:01:22 +0100

    Added more content #4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,20 +14,19 @@
     <section class="about py-5">
         <div class="container">
             <div class="d-block row d-md-flex flex-md-row py-4">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="{{ asset( '/img/logo.png' ) }}" alt="">
+                <div class="col-md-5 d-flex align-items-center justify-content-center">
+                    <img class="img-fluid" src="{{ asset( '/img/logo.png' ) }}" alt="">
                 </div>
                 <br class="d-block d-md-none">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="col-md-7 d-flex align-items-center justify-content-center">
                     <div class="text-block">
                         <h3>Linda Carlstad</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                            officia deserunt mollit anim id est laborum.
+                            LINDA är en studentförening under Karlstads Universitet
+                            LINDA bevakar ditt intresse som student, såväl att du har det bra på
+                            universitet som på fritiden, utbildningen kommer inte alltid att vara
+                            rolig eller lätt och då hoppas vi att vi kan vara till din hjälp med studierna
+                            om det så krävs eller med att bara ta det lugnt och umgås...
                         </p>
                         <a class="btn btn-grey btn-expand" href="{{ route( 'about' ) }}">Läs mer</a>
                     </div>
@@ -35,39 +34,42 @@
             </div>
             <hr>
             <div class="d-block row d-md-flex flex-md-row-reverse py-4">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="{{ asset( '/img/logo.png' ) }}" alt="">
+                <div class="col-md-5 d-flex align-items-center justify-content-center">
+                    <img class="img-fluid" src="{{ asset( '/img/logo.png' ) }}" alt="">
                 </div>
                 <br class="d-block d-md-none">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="col-md-7 d-flex align-items-center justify-content-center">
                     <div class="text-block">
-                        <h3>Linda Carlstad</h3>
+                        <h3>Tentahub</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                            officia deserunt mollit anim id est laborum.
+                            Som student vid Karlstads Universitet kan du köpa svaren på en tenta
+                            via biblioteket för 50 KR.
+                            <br>
+                            <b>Det tycker vi är dumt</b>
+                            <br>
+                            I Linda Vill vi att svaren på tentorna ska finnas tillgängliga gratis
+                            för alla våra medlemmar och för att lyckas med det behöver vi din hjälp!
                         </p>
-                        <a class="btn btn-grey btn-expand" href="{{ route( 'about' ) }}">Läs mer</a>
+                        <a class="btn btn-grey btn-expand" href="http://tentahub.se/linda">Läs mer</a>
                     </div>
                 </div>
             </div>
             <hr>
             <div class="d-block row d-md-flex flex-md-row py-4">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="{{ asset( '/img/logo.png' ) }}" alt="">
+                <div class="col-md-5 d-flex align-items-center justify-content-center">
+                    <img class="img-fluid" src="{{ asset( '/img/bunkern.png' ) }}" alt="">
                 </div>
                 <br class="d-block d-md-none">
-                <div class="col-md-6 d-flex align-items-center justify-content-center">
+                <div class="col-md-7 d-flex align-items-center justify-content-center">
                     <div class="text-block">
-                        <h3>Linda Carlstad</h3>
+                        <h3>Studentpuben Bunkern</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                            Bunkern är Karlstads äldsta studentpub av studenter, för studenter på
+                            Karlstad Universitet. Endast en kort promenad från Universitetet i
+                            bostadsområdet Duvkulla. Placerat i ett cykelförråd som byggdes om för
+                            många år sedan till en mysig studentpub med bra priser.
                         </p>
-                        <a class="btn btn-grey btn-expand" href="{{ route( 'about' ) }}">Läs mer</a>
+                        <a class="btn btn-grey btn-expand" href="http://www.studentpuben-bunkern.se/">Läs mer</a>
                     </div>
                 </div>
             </div>
