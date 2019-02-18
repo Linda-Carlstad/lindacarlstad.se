85ce6a191f56e6c8e5287dae6654f5107211b66c
Felix Wetell <felix.wetell@gmail.com>
2019-02-18 18:17:15 +0100

    Update profile structure

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -9,66 +9,69 @@
 @endif
 
     <div class="text-center">
-        <h4>{{ Auth::user()->name }}</h4>
+        <h3>{{ Auth::user()->name }}</h3>
     </div>
-
+    <hr>
+    <h4>Ändra namn</h4>
     <form class="" action="/user/{{ Auth::user()->id }}" method="post">
         @csrf
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="name">
-        <label for="name">Name:</label>
-        <br>
-        <input type="text" name="name" value="" placeholder="Name">
-        <br>
-        <br>
-        <button type="submit">Send</button>
+        <div class="form-group">
+            <label for="name">Namn:</label>
+            <input type="text" class="form-control col-md-6" name="name" value="" placeholder="Name">
+        </div>
+        <button type="submit" class="btn btn-grey btn-expand">Send</button>
     </form>
+    <br>
+    <h4>Ändra email</h4>
     <hr>
     <form class="" action="/user/{{ Auth::user()->id }}" method="post">
         @csrf
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="email">
-        <label for="email">Email:</label>
-        <br>
-        <input type="text" name="email" value="" placeholder="email">
-        <br>
-        <label for="emailPassword">Current Password:</label>
-        <br>
-        <input type="password" name="emailPassword" value="" placeholder="password">
-        <br>
-        <br>
-        <button type="submit">Send</button>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" class="form-control col-md-6" name="email" value="" placeholder="Email">
+        </div>
+
+        <div class="form-group">
+            <label for="emailPassword">Current Password:</label>
+            <input type="password" class="form-control col-md-6" name="emailPassword" value="" placeholder="Lösenord">
+        </div>
+        <button type="submit" class="btn btn-grey btn-expand">Send</button>
     </form>
     <hr>
+    <h4>Ändra lösenord</h4>
     <form class="" action="/user/{{ Auth::user()->id }}" method="post">
         @csrf
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="password">
-        <label for="currentPassword">Current Password:</label>
-        <br>
-        <input type="password" name="currentPassword" value="" placeholder="password">
-        <br>
-        <label for="password">New Password:</label>
-        <br>
-        <input type="password" name="password" value="" placeholder="password">
-        <br>
-        <label for="confirmPassword">New Password:</label>
-        <br>
-        <input type="password" name="confirmPassword" value="" placeholder="password">
-        <br>
-        <br>
-        <button type="submit">Send</button>
+        <div class="form-group">
+            <label for="currentPassword">Current Password:</label>
+            <input type="password" class="form-control col-md-6" name="currentPassword" value="" placeholder="Lösenord">
+        </div>
+        <div class="form-group">
+            <label for="password">New Password:</label>
+            <input type="password" class="form-control col-md-6" name="password" value="" placeholder="Lösenord">
+        </div>
+        <div class="form-group">
+            <label for="confirmPassword">New Password:</label>
+            <br>
+            <input type="password" class="form-control col-md-6" name="confirmPassword" value="" placeholder="Lösenord">
+        </div>
+        <button type="submit" class="btn btn-grey btn-expand">Send</button>
     </form>
     <hr>
+    <h4>Ändra personnummer</h4>
     <form class="" action="/user/{{ Auth::user()->id }}" method="post">
         @csrf
         {{ method_field( 'patch' ) }}
         <input type="hidden" name="type" value="idNumber">
-        <label for="idNumber">Personnummer:</label>
-        <br>
-        <input type="text" name="idNumber" value="" placeholder="password">
-        <br>
-        <br>
-        <button type="submit">Send</button>
+        <div class="form-group">
+            <label for="idNumber">Personnummer:</label>
+            <input type="text" class="form-control col-md-6" name="idNumber" value="" placeholder="Lösenord">
+        </div>
+        <button type="submit" class="btn btn-grey btn-expand">Send</button>
     </form>
 @endsection
