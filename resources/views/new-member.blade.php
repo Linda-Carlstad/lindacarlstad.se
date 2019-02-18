ca0105145ed2727c8e4e840ad8a5b86e9e030d6a
Mackeboi <36602678+Mackeboi@users.noreply.github.com>
2019-02-18 18:11:52 +0100

    Created new-member view #19

--- resources/views/new-member.blade.php
+++ resources/views/new-member.blade.php
@@ -3,8 +3,23 @@
 
 @section('content')
 
-<div class="">
-
-</div>
+  <div class="d-block row d-md-flex flex-md-row py-4">
+      <div class="col-md-5 d-flex align-items-center justify-content-center">
+          <img class="img-fluid" src="{{ asset( '/img/logo.png' ) }}" alt="">
+      </div>
+      <br class="d-block d-md-none">
+      <div class="col-md-7 d-flex align-items-center justify-content-center">
+          <div class="text-block">
+              <h3>Bli medlem</h3>
+              <p>
+                Som medlem i Linda kommer du kunna gå på olika olika evengemang, som snits, nollning eller sittningar.
+                Du måste även vara medlem i Linda för att kunna ha möjligheten till att köpa en ovve.
+                Medlemskapet kostar 300 kr och gäller livet ut, det tillkommer även en serviceavgift på 15kr hos tictail.
+              </p>
+              <a class="btn btn-grey btn-expand" href="http://lindacarlstad.tictail.com/product/medlemskap">Köp  </a>
+          </div>
+      </div>
+  </div>
+  <hr>
 
 @endsection
