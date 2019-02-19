81476840becd6ab7ebf9c2942476aa4de90f6c55
Felix Wetell <felix.wetell@gmail.com>
2019-02-19 13:23:05 +0100

    fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('queue-work')
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
