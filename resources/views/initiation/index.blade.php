f5249ae19955083012c8418dc39ce936a303181e
Felix Wetell <felix.wetell@gmail.com>
2019-03-20 15:39:03 +0100

    Initiation fixes #42

--- resources/views/initiation/index.blade.php
+++ resources/views/initiation/index.blade.php
@@ -2,7 +2,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Nollning</h1>
+    <h1>Nollning {{ date( 'Y' ) }}</h1>
+    <hr>
+    <div class="text-center">
+        <h2>Snabbval</h3>
+        <a class="btn btn-lg btn-grey">Schema</a>
+        <a class="btn btn-lg btn-grey">Regler</a>
+    </div>
+    <hr>
+    <div class="updates">
+        <div class="text-center">
+            <h3>Uppdateringar</h3>
+            <p>Inga updateringar finns att visa</p>
+        </div>
+    </div>
     <hr>
     <div class="d-block row d-md-flex flex-md-row py-4">
         <div class="col-md-5 d-flex align-items-center justify-content-center">
@@ -11,7 +24,7 @@
         <br class="d-block d-md-none">
         <div class="col-md-7 d-flex align-items-center justify-content-center">
             <div class="text-block">
-                <h3>Linda Carlstad</h3>
+                <h3>Nollning med Linda Carlstad</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -33,7 +46,12 @@
             <div class="text-block">
                 <h3>Schema</h3>
                 <p>
-                    Titta på året nollningsschema
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
                 </p>
                 <a class="btn btn-grey" href="{{ route( 'schedule' ) }}">Till schema</a>
             </div>
@@ -42,12 +60,12 @@
     <hr>
     <div class="d-block row d-md-flex flex-md-row py-4">
         <div class="col-md-5 d-flex align-items-center justify-content-center">
-            <img class="img-fluid" src="{{ asset('/img/logo.png' ) }}" alt="Studentpuben Bunkern logga">
+            <img class="img-fluid" src="{{ asset('/img/logo.png' ) }}" alt="Linda Carlstad logga med text">
         </div>
         <br class="d-block d-md-none">
         <div class="col-md-7 d-flex align-items-center justify-content-center">
             <div class="text-block">
-                <h3>Linda Carlstad</h3>
+                <h3>Regler</h3>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -59,4 +77,5 @@
             </div>
         </div>
     </div>
+
 @endsection
