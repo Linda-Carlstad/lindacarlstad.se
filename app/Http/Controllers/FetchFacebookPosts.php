9c0b4560704f332cd54d72725cfac0c3b60b5348
Felix Wetell <felix.wetell@gmail.com>
2019-03-21 17:47:30 +0100

    Small fixes

--- app/Http/Controllers/FetchFacebookPosts.php
+++ app/Http/Controllers/FetchFacebookPosts.php
@@ -22,14 +22,15 @@ class FetchFacebookPosts extends Controller
       require_once '../vendor/facebookAPI/src/Facebook/autoload.php';
 
       $fb = new Facebook([
-      'app_id' => '770437356636722',
-      'app_secret' => '62e0185cc65a820d038df51ce1592d8c',
-      'default_graph_version' => 'v3.2',
+          'app_id' => '770437356636722',
+          'app_secret' => '62e0185cc65a820d038df51ce1592d8c',
+          'default_graph_version' => 'v3.2',
       ]);
 
       try {
       // Returns a `Facebook\FacebookResponse` object
-      $response = $fb->get('/1513583672218443?fields=posts{caption,full_picture,message,permalink_url,created_time}', 'EAAK8tWz2yjIBAOXEQy3D96dW3HtZBKmBOzDKu9hbkHNUWgrjXaLNShJ0L9Ekpb0Fanrsw8aWv3w6X4Up2nhTFHZASyVMoJXSt0ZCJOHBF94JOIpdSyGa2SH1TtHlesHCWeJWhOWj74E837rOswgMXWqpyWzuFYZD');
+      $response = $fb->get('/1513583672218443?fields=posts{caption,full_picture,message,permalink_url,created_time}',
+        'EAAK8tWz2yjIBAOXEQy3D96dW3HtZBKmBOzDKu9hbkHNUWgrjXaLNShJ0L9Ekpb0Fanrsw8aWv3w6X4Up2nhTFHZASyVMoJXSt0ZCJOHBF94JOIpdSyGa2SH1TtHlesHCWeJWhOWj74E837rOswgMXWqpyWzuFYZD');
       } catch(Facebook\Exceptions\FacebookResponseException $e) {
       echo 'Graph returned an error: ' . $e->getMessage();
       exit;
@@ -42,7 +43,7 @@ class FetchFacebookPosts extends Controller
 
       $posts_data = $posts->getProperty('posts');
 
-      return view('news')->with('facebookData', $posts_data);
+      return view( 'news' )->with('facebookData', $posts_data);
 
     }
 }
