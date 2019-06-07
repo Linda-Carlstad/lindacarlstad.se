f829ec5afe4f9a62386cb25ed2d69b6737d94e64
Felix Wetell <felix.wetell@gmail.com>
2019-06-07 11:30:08 +0200

    Temp removal of Facebook news feed

--- app/Http/Controllers/FetchFacebookPosts.php
+++ app/Http/Controllers/FetchFacebookPosts.php
@@ -19,30 +19,32 @@ class FetchFacebookPosts extends Controller
     public function __invoke(Request $request)
     {
 
-      require_once '../vendor/facebookAPI/src/Facebook/autoload.php';
+      // require_once '../vendor/facebookAPI/src/Facebook/autoload.php';
+      //
+      // $fb = new Facebook([
+      // 'app_id' => '105287106191432',
+      // 'app_secret' => '25b0be5edaede0f1b58df56019462dbc',
+      // 'default_graph_version' => 'v3.3',
+      // ]);
+      //
+      // try {
+      // // Returns a `Facebook\FacebookResponse` object
+      // $response = $fb->get('/1513583672218443?fields=posts{caption,full_picture,message,permalink_url,created_time}', 'EAAK8tWz2yjIBABfdDZCYh6wQ3aB8sxJZCEZBf9nnFdKsCrrUn3JBLDa2ZBZBINb7yNwqwsK1uQkHX5w9CEXTIlvLIMptNS5ruLQ7UAO3rvT64TPZCOF0U7q7j2GMzqL9gQiJduUbnezR3msQZAuoMbKUdALcyhTojvDOxCxdl0wUkcqZCnhgXSIh4k7FvofcBflrk1asrmsZAHAZDZD');
+      // } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // echo 'Graph returned an error: ' . $e->getMessage();
+      // exit;
+      // } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      // exit;
+      // }
+      //
+      // $posts = $response->getGraphNode();
+      //
+      // $posts_data = $posts->getProperty('posts');
+      //
+      //return view('news')->with('facebookData', $posts_data);
 
-      $fb = new Facebook([
-      'app_id' => '105287106191432',
-      'app_secret' => '25b0be5edaede0f1b58df56019462dbc',
-      'default_graph_version' => 'v3.3',
-      ]);
-
-      try {
-      // Returns a `Facebook\FacebookResponse` object
-      $response = $fb->get('/1513583672218443?fields=posts{caption,full_picture,message,permalink_url,created_time}', 'EAAK8tWz2yjIBABfdDZCYh6wQ3aB8sxJZCEZBf9nnFdKsCrrUn3JBLDa2ZBZBINb7yNwqwsK1uQkHX5w9CEXTIlvLIMptNS5ruLQ7UAO3rvT64TPZCOF0U7q7j2GMzqL9gQiJduUbnezR3msQZAuoMbKUdALcyhTojvDOxCxdl0wUkcqZCnhgXSIh4k7FvofcBflrk1asrmsZAHAZDZD');
-      } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      echo 'Graph returned an error: ' . $e->getMessage();
-      exit;
-      } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
-      exit;
-      }
-
-      $posts = $response->getGraphNode();
-
-      $posts_data = $posts->getProperty('posts');
-
-      return view('news')->with('facebookData', $posts_data);
+      return view( 'news' );
 
     }
 }
