81ceafcc17c55afc79f8211ba8435bc54277e2f6
Felix Wetell <felix.wetell@gmail.com>
2019-08-17 13:40:46 +0200

    Closes #63, initializing slick

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,13 @@ $(document).ready( function ()
 
 });
 
-$('#documentTab a').on('click', function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+    $( '.slick-slider' ).slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      mobileFirst: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      centerMode: true
+    });
+});
