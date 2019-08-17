9998a9b8cf5ff7205b8eae92a35862af2f88836d
Felix Wetell <felix.wetell@gmail.com>
2019-08-17 17:21:25 +0200

    Added ability to have cusom messages for each error #67

--- public/js/error.js
+++ public/js/error.js
@@ -3,6 +3,16 @@ if( $( "#error-message" ) )
     let error = $( '#error-id' ).text();
     let message = '> Något har gått fel. <br> > Vi kanske drack för mycket guck? ^1000 <br>> <br> > Vänligen testa igen.'
 
+    switch( parseInt( error ) )
+    {
+        case 404:
+            message = '> Du har angett en felatkig länk. <br> > Är du full eller? ^1000 <br>> <br> > Försök med en annan länk. <br> > :)';
+            break;
+        default:
+            message
+            break;
+
+    }
     var typed = new Typed( "#error-message",
     {
         strings:
