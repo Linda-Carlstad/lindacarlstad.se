a5f432cb09412cd2e151e30dec4d95529e5a6eff
Felix Wetell <felix.wetell@gmail.com>
2019-08-17 13:40:58 +0200

    Refactored JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready( function ()
 {
     // Enabling Bootstrap Tooltips
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
+    $( function () {
+      $( '[data-toggle="tooltip"]' ).tooltip()
     })
 
-});
+    $( '#documentTab a' ).on( 'click', function( e )
+    {
+      e.preventDefault()
+      $( this ).tab( 'show' )
+    } );
 
     $( '.slick-slider' ).slick({
       autoplay: true,
