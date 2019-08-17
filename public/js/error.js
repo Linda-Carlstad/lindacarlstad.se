a7e8456f3cbf6c083bb68565265b8cee420b2235
Felix Wetell <felix.wetell@gmail.com>
2019-08-17 17:39:11 +0200

    Closes #67 , set custom messages for every error code

--- public/js/error.js
+++ public/js/error.js
@@ -1,15 +1,34 @@
 if( $( "#error-message" ).length )
 {
     let error = $( '#error-id' ).text();
-    let message = '> Något har gått fel. <br> > Vi kanske drack för mycket guck? ^1000 <br>> <br> > Vänligen testa igen.'
+    let message = '';
 
     switch( parseInt( error ) )
     {
+        case 401:
+            message = '> Du är för full för att göra det där. <br> > Funderat på att nyktra till? ^1000 <br>> <br> > Eller gå till Bunkern och sup mer.';
+            break;
+        case 403:
+            message = '> Här får du inte vara. <br> > Försöker du ta vår guck? ^1000 <br>> <br> > Försök inte igen, då blir Trulle ledsen.';
+            break;
         case 404:
             message = '> Du har angett en felatkig länk. <br> > Är du full eller? ^1000 <br>> <br> > Försök med en annan länk. <br> > :)';
             break;
+        case 419:
+            message = '> Festen är slut. <br> > Har Securitas kommit än? ^1000 <br>> <br> > Gå och lägg dig, det är sent. <br> > :)';
+            break;
+        case 429:
+            message = '> Nu får du sakta ner litegrann. <br> > Kom du inte in på Bunkern ikväll? ^1000 <br>> <br> > Nyktra till och försök igen om ett tag. <br> > :)';
+            break;
+        case 500:
+            message = '> Servern är full, blev bjuden på guck. <br> > Vem? ^1000 <br>> <br> > Låt servern nyktra till och försök igen om ett tag. <br> > :)';
+            break;
+        case 503:
+            message = '> Gucken tog ner servern. <br> > Vem söp ner den? ^1000 <br>> <br> > Våra onyktra tekniker kollar på felet. <br> > :)';
+            break;
+
         default:
-            message
+            message =  '> Något har gått fel. <br> > Vi kanske drack för mycket guck? ^1000 <br>> <br> > Vänligen testa igen.';
             break;
 
     }
