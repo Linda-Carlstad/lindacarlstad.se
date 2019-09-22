237d6567ba36b57cf6501dbe1bf36097cc9c6350
Felix Wetell <felix.wetell@gmail.com>
2019-09-22 23:05:49 +0200

    Fixes #58

--- resources/views/it-committee.blade.php
+++ resources/views/it-committee.blade.php
@@ -14,9 +14,12 @@
             <div class="text-block">
                 <h3>Vilka är it-kommittén?</h3>
                 <p>
-                    It-kommittén är blah blh blah...
+                    Linda Carlstads it-kommitté är utskott från styrelsen som har hand om allt som rör IT inom föreningen.
+                    Vi underhåller alla webbsidor som föreningen har, fyller på med inehåll, rättar till fel och nyutvecklar såklart.
+                    Nyligen har vi fått in ett flertal ny medlemmar till it-kommittén och vi vill börja utveckla mer än webbsidor.
+                    Låter det intressant så hör av dig till oss så löser vi något till dig!
                 </p>
-                <a class="btn btn-grey btn-expand" href="{{ route('about' ) }}">Läs mer</a>
+                <a class="btn btn-grey btn-expand" target="_blank" href="https://github.com/Linda-Carlstad">Lär dig mer</a>
             </div>
         </div>
     </div>
@@ -28,11 +31,16 @@
         <br class="d-block d-md-none">
         <div class="col-md-7 d-flex align-items-center">
             <div class="text-block">
-                <h3>Gå med i it-kommittén</h3>
+                <h3>Gå med i it-kommittén!</h3>
                 <p>
-                    Vill du gå med it-kommittén?
+                    Går du in tankarna och vill göra något mer än bara skoluppgifterna? Vi i Lindas it-kommitté välkommnar dig med
+                    öppna armar. Vi har massor med projekt hela tiden och har alltid saker att göra. Gillar du front-end utveckling, back-end it-kommittén,
+                    eller du kanske har en brinnande passion för apputveckling. Vad det än är så har vi platsen och resurserna
+                    för dina ideér. På vår <a class="link text-underline" target="_blank" href="https://github.com/Linda-Carlstad">Github</a> hittar du alla pågående projekt.
+                    <br><br>
+                    Så vad väntar du på? Hör av dig så löser vi något kul att göra.
                 </p>
-                <a class="btn btn-grey btn-expand" href="mailto:it@lindacarlstad.se">Skriv till oss</a>
+                <a class="btn btn-grey btn-expand" href="{{ route( 'contact' ) }}">Kontakt</a>
             </div>
         </div>
     </div>
