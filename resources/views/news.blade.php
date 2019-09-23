be84aeb4f77eb6856e15c3388d1bd89e0607884b
Felix Wetell <felix.wetell@gmail.com>
2019-09-23 10:39:06 +0200

    Updated news text placement #84

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -5,6 +5,8 @@
 
 <h1 class="text-center">Nyheter</h1>
 <hr>
+<div class="text-center">
+    <p>Gå gärna in på vår Facebook, <a target="_blank" class="link" href="https://www.facebook.com/linda.carlstad/">Linda Carlstad</a>.</p>
 </div>
 
 <hr>
