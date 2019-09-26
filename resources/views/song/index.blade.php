b8f752660352d250d25e966706f5070d2a2b2d0f
Felix Wetell <felix.wetell@gmail.com>
2019-09-26 15:37:05 +0200

    Fixes #91, added ability to seach for songs

--- resources/views/song/index.blade.php
+++ resources/views/song/index.blade.php
@@ -8,14 +8,40 @@
         <p>
             En samling med sånger som vi i LINDA sjunger, på nollning och inte.
             <br><br>
-            Antal sånger: {{ $songs->count() }}
+            Antal sånger: {{ $total }}
         </p>
+
+        @if( !$songs->isEmpty() || isset( $search ) )
+            <form class="col-md-8 offset-md-2 mt-2" action="{{ '/sånger' }}" method="get">
+                @csrf
+
+                <div class="form-group row">
+                    <div class="input-group">
+                        <input id="search" type="text" placeholder="Sök.." class="form-control{{ $errors->has('search') ? ' is-invalid' : '' }}" name="search" value="{{ isset( $search ) ? $search : "" }}" autofocus>
+                        @if ($errors->has('search'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('search' ) }}</strong>
+                            </span>
+                        @endif
+                        <div class="input-group-append">
+                            <button type="submit" name="button" class="btn btn-grey btn-file">Sök</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        @endif
+    </div>
+    <div class="text-center">
+        @if( isset( $search ) )
+            <h4>Du sökte på: <i>{{ $search }}</i></h4>
+            <h5>Antal resultat: <i>{{ $songs->count() }}</i></h5>
+        @endif
     </div>
     <hr>
     <div class="d-block row d-md-flex flex-md-row-reverse py-4">
         <div class="col-12 text-center">
             @if( $songs->isEmpty() )
-                <p class="text-center">Inga sånger tillagda.</p>
+                <p class="text-center">Inga sånger hittade.</p>
             @endif
             <ul class="list-group col-md-8 offset-md-2">
 
@@ -27,5 +53,15 @@
             </ul>
         </div>
     </div>
+
+    <div class="d-flex justify-content-center">
+        {{ $songs->links() }}
+    </div>
+    @if( isset( $search ) )
+        <hr>
+        <div class="text-center">
+            <a class="btn btn-grey btn-expand" href="{{ route( 'song' ) }}">Visa alla</a>
+        </div>
+    @endif
 
 @endsection
