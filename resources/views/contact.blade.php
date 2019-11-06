cec735f1fc10ce8b86e37674af852876d658e1f3
Felix Wetell <felix.wetell@gmail.com>
2019-11-06 16:40:36 +0100

    Rework of contact view #90

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,65 +4,76 @@
 @section('content')
 
     <h1 class="text-center">Kontakta oss</h1>
-    <br>
-    <div class="card bg-dark">
-        <div class="card-body">
-            <form method="POST" action="/contactForm">
-                @csrf
+    <hr>
+    <div class="py-4">
+        <form method="POST" action="/contactForm">
+            @csrf
+            <div class="form-group row">
+                <label for="name" class="col-sm-4 col-form-label text-md-right">Namn</label>
 
-                <div class="form-group row">
-                    <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Namn') }}</label>
+                <div class="col-md-6">
+                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                    @if ($errors->has('name'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                    @endif
                 </div>
+            </div>
 
-                <div class="form-group row">
-                    <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Email') }}</label>
+            <div class="form-group row">
+                <label for="subject" class="col-sm-4 col-form-label text-md-right">Ämne</label>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                <div class="col-md-6">
+                    <input id="subject" type="subject" class="form-control{{ $errors->has('subject') ? ' is-invalid' : '' }}" name="subject" value="{{ old('subject') }}" required autofocus>
 
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                    @if ($errors->has('subject'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('subject') }}</strong>
+                        </span>
+                    @endif
                 </div>
+            </div>
 
-                <div class="form-group row">
-                    <label for="Meddelande" class="col-sm-4 col-form-label text-md-right">{{ __('Meddelande') }}</label>
+            <div class="form-group row">
+                <label for="email" class="col-sm-4 col-form-label text-md-right">Email</label>
 
-                    <div class="col-md-6">
-                        <textarea id="Meddelande" type="textarea" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required></textarea>
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <div class="col-md-8 offset-md-4">
-                        <small class="text-muted">Vi sparar inga uppgifter.</small>
-                    </div>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
                 </div>
-                <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-4">
-                      <button type="submit" class="btn btn-red">
-                          {{ __('Skicka') }}
-                      </button>
-                  </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="message" class="col-sm-4 col-form-label text-md-right">Meddelande</label>
+
+                <div class="col-md-6">
+                    <textarea id="message" type="textarea" class="form-control{{ $errors->has('message') ? ' is-invalid' : '' }}" name="message" value="{{ old('message') }}" required></textarea>
+
+                    @if ($errors->has('message'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('message') }}</strong>
+                        </span>
+                    @endif
                 </div>
-            </form>
-        </div>
+                <div class="col-md-8 offset-md-4">
+                    <small>Vi sparar inga uppgifter.</small>
+                </div>
+            </div>
+            <div class="form-group row mb-0">
+                <div class="col-md-8 offset-md-4">
+                  <button type="submit" class="btn btn-grey">
+                      {{ __('Skicka') }}
+                  </button>
+              </div>
+            </div>
+        </form>
     </div>
 
 @endsection
