e510d9fd92d1f23e77513b21e18e6d81c80c30d6
Felix Wetell <felix.wetell@gmail.com>
2019-11-08 10:29:40 +0100

    Adds flash messages to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -152,6 +152,23 @@
             <div class="container-fluid mt-5 py-4 py-md-5">
                 <div class="row justify-content-center">
                     <div class="col-md-8">
+                        @if( session()->has( 'success' ) )
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                {{ session()->get( 'success' ) }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+
+                        @if( session()->has( 'error' ) )
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{ session()->get( 'error' ) }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
                         @yield( 'content' )
                         <br>
                     </div>
