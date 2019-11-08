623a66f4a51b83634a058f8b5ab8ddc964a83ef2
Felix Wetell <felix.wetell@gmail.com>
2019-11-08 15:04:42 +0100

    Fixes #97

--- resources/views/vendor/pagination/simple-bootstrap-4.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-4.blade.php
@@ -3,22 +3,22 @@
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="page-item disabled" aria-disabled="true">
-                <span class="page-link">@lang('pagination.previous')</span>
+                <span class="page-link">« Föregående</span>
             </li>
         @else
             <li class="page-item">
-                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">{{ '<<' }} Föregående</a>
+                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">« Föregående</a>
             </li>
         @endif
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item">
-                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Nästa >></a>
+                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">Nästa »</a>
             </li>
         @else
             <li class="page-item disabled" aria-disabled="true">
-                <span class="page-link">Nästa</span>
+                <span class="page-link">Nästa »</span>
             </li>
         @endif
     </ul>
