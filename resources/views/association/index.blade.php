9b42f8da59242fb61ffbbbc590886ca5ccc07fc2
Felix Wetell <felix.wetell@gmail.com>
2020-01-03 22:26:25 +0100

    Typo on associations

--- resources/views/association/index.blade.php
+++ resources/views/association/index.blade.php
@@ -1,21 +1,21 @@
-@section('title', 'Partners')
+@section('title', 'Studentföreningar')
 @extends('layouts.app')
 
 @section('content')
-    <h1>Föreningar</h1>
+    <h1>Studentföreningar</h1>
     <hr>
     <div class="col-12">
         <p>
-            Här finner du alla föreningar på Karlstads Universitet.
+            Här finner du alla studentföreningar på Karlstads Universitet.
             <br><br>
-            Antal förenigar: {{ $associations->count() }}
+            Antal studentföreningar: {{ $associations->count() }}
         </p>
     </div>
     <hr>
     <div class="d-block row d-md-flex flex-md-row-reverse py-4">
         <div class="col-12 text-center">
             @if( $associations->isEmpty() )
-                <p class="text-center">Inga förenigar tillagda.</p>
+                <p class="text-center">Inga studentföreningar tillagda.</p>
             @endif
             <ul class="list-group col-md-8 offset-md-2">
                 @foreach( $associations as $association )
