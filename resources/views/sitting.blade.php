b9f528fee8683ae77e69825e71e8936d59211b67
Felix Wetell <felix.wetell@gmail.com>
2020-01-04 18:54:09 +0100

    Adds view for sitting #83

---
+++ resources/views/sitting.blade.php
@@ -0,0 +1,93 @@
+@section( 'title', 'Sittning' )
+@extends( 'layouts.app' )
+@section( 'content' )
+
+    <h1>Sittning</h1>
+    <hr>
+    <div class="d-block row d-md-flex flex-md-row py-4">
+        <div class="col-md-5 d-flex align-items-center justify-content-center">
+            <img class="img-fluid" src="{{ asset('/img/people.svg' ) }}" alt="Linda Carlstad medlemmar och studenter">
+        </div>
+        <br class="d-block d-md-none">
+        <div class="col-md-7 d-flex align-items-center">
+            <div class="text-block">
+                <h3>Vad är en sittning?</h3>
+                <p>
+                    Linda Carlstads it-kommitté är utskott från styrelsen som har hand om allt som rör IT inom föreningen.
+                    Vi underhåller alla webbsidor som föreningen har, fyller på med innehåll, rättar till fel och nyutvecklar.
+                    Nyligen har vi fått in ett flertal ny medlemmar till it-kommittén och vi vill börja utveckla mer än webbsidor.
+                    Låter det intressant så hör av dig till oss så hjälper vi dig att komma igång!
+                </p>
+                <hr>
+                <h3>Sånger på sittning</h3>
+                <div class="text-center text-md-left">
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/portos-visa">Portos visa</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/lambo">Lambo</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/jag-ska-festa">Jag ska festa</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/linda-i-baren">Linda i Baren</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/portos-visa">Portos visa</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/strejk-pa-pripps">Strek på Prips</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/ack-varmland">Ack, Värmland</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/feta-fransyskor">Feta fransyskor</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/bordeaux-bordeaux">Bordeaux Bordeaux</a>
+                    <a class="btn btn-grey m-1" target="_blank" href="https://lindacarlstad.se/s%C3%A5nger/spritbolaget">Spritbolaget</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="text-center">
+        <h3>Regler på sittning</h3>
+    </div>
+    <ul class="list-group">
+        <li class="list-group-item bg-dark">
+            1. När toastmastern talar är man TYST. När toastmastern påkallar uppmärksamhet har man tre ord
+            på sig att avsluta pågående samtal. Dock om det är så att man har halva inne, må man nyttja
+            fem ord för att avsluta konversationen.
+        </li>
+        <li class="list-group-item bg-dark">
+            2. När man tycker att toastmastern bör ta upp en sång påkallar man dennes uppmärksamhet
+            med: TEMPO!
+        </li>
+        <li class="list-group-item bg-dark">
+            3. När toastmastern tar upp en sång sjunger man; samma sång som toastmastern!
+        </li>
+        <li class="list-group-item bg-dark">
+            4. Den som ej har medfödd sångröst äga ändå rätt att efter bästa förmåga delta i sången.
+            Alla är dock skyldiga att sjunga hellre än bra.
+        </li>
+        <li class="list-group-item bg-dark">
+            5. Brott mot tyngdlagen undanbedes.
+        </li>
+        <li class="list-group-item bg-dark">
+            6. De som känner sig osäkra ifråga om sången borde hålla sig en takt före, så att när slutet
+            av sången nås, ingen blir efter.
+        </li>
+        <li class="list-group-item bg-dark">
+            7. Om man vill sjunga en sång på egen hand må detta endast i undantagsfall göras och i så fall
+            ska detta undantagslöst göras under bordet.
+        </li>
+        <li class="list-group-item bg-dark">
+            8. Det är strängeligen förbjudet att behålla sitt goda humör för sig själv!
+        </li>
+        <li class="list-group-item bg-dark">
+            9. Intagen föda återtages ej.
+        </li>
+        <li class="list-group-item bg-dark">
+            10. När maten är slut på din egen tallrik, fortsätt på den till höger.
+        </li>
+        <li class="list-group-item bg-dark">
+            11. Dörrar med höga trösklar kallas fönster, dem får man inte gå ut genom.
+        </li>
+        <li class="list-group-item bg-dark">
+            12. Det är ej rekommenderat att luta sig över bordet när rummet flyttar sig i en cirkel.
+        </li>
+        <li class="list-group-item bg-dark">
+            13. Kontrollera att era bordsgrannar ej nyttjar rusdrycker så det får suddiga ansikten.
+        </li>
+        <li class="list-group-item bg-dark">
+            14. Under bordet: Håll till vänster!
+        </li>
+    </ul>
+
+@endsection
