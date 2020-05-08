67c1024aaa028e97ea84b59b0fd3a800194455bf
Felix Wetell <felix.wetell@gmail.com>
2020-05-08 09:29:02 +0200

    Adds app slogan in config #114

--- config/app.php
+++ config/app.php
@@ -17,6 +17,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Slogan
+    |--------------------------------------------------------------------------
+    |
+    | This value is the slogan of your application. This value is used when the
+    | framework needs to place the application's slogan in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'slogan' => env('APP_SLOGAN', 'Laravel'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Application Environment
     |--------------------------------------------------------------------------
     |
