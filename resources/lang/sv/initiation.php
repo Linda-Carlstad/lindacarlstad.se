2f6ae098a6fea66a8bf2561d77d1d8ddff4bffa8
Felix Wetell <felix.wetell@gmail.com>
2020-06-10 12:57:05 +0200

    Adds translation files #129

---
+++ resources/lang/sv/initiation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'year' => 'år',
+    'description' => 'beskrivning',
+    'price' => 'pris',
+    'show_price' => 'visa pris',
+    'facebook_group' => 'Facebook-grupp',
+    'playlist' => 'spellista',
+
+    'title' => 'dagens namn',
+    'extra' => 'extra information',
+    'date' => 'datum',
+    'time' => 'tid',
+    'location' => 'plats',
+
+    'name' => 'namn',
+    'rank' => 'rank',
+    'email' => 'e-postadress',
+    'phone' => 'telefonnummer',
+
+    'initiation_id' => 'nollningsår',
+];
---
+++ resources/lang/sv/status.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'edit' => 'uppdatering',
+    'create' => 'skapad',
+    'delete' => 'borttagen',
+];
