6b7aa0c934b8763433a6273fd1fb0a8c14c4084e
Felix Wetell <felix.wetell@gmail.com>
2020-07-17 14:43:13 +0300

    Updates navbar anchor links #109 #114

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -4,7 +4,7 @@
 <!-- End Google Tag Manager (noscript) -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top scrolling-navbar d-none d-md-block">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url( '/' ) }}" aria-label="Home screen">
+        <a class="navbar-brand" href="{{ url( '/' ) }}" aria-label="Start" title="Start">
             <img class="" src="{{ asset( 'img/small-logo.png') }}" alt="Golden apple with crown">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,32 +13,32 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="nav navbar-nav">
                 <li class="nav-item {{ Request::is('/') ? 'active' : null }}">
-                    <a class="nav-link" href="{{ url( '/' ) }}">Start<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ url( '/' ) }}" title="Start">Start<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'program' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'programs' ) }}">Program</a>
+                    <a class="nav-link" href="{{ route( 'programs' ) }}" title="Program">Program</a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'föreningen' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'about' ) }}">Föreningen</a>
+                    <a class="nav-link" href="{{ route( 'about' ) }}" title="Föreningen">Föreningen</a>
                 </li>
                 @if( session()->has( 'initiation' ) )
                     <li class="nav-item {{ Request::segment(1) === 'nollning' && Request::segment(2) === session()->get( 'initiation' ) ? 'active' : null }}">
-                        <a class="nav-link" href="{{ route( 'initiation.show', session()->get( 'initiation' ) ) }}">Nollning {{ session()->get( 'initiation' ) }}</a>
+                        <a class="nav-link" href="{{ route( 'initiation.show', session()->get( 'initiation' ) ) }}" title="Nollning {{ session()->get( 'initiation ) }}">Nollning {{ session()->get( 'initiation' ) }}</a>
                     </li>
                 @endif
             </ul>
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item {{ Request::segment(1) === 'sånger' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'song' ) }}">Sånger</a>
+                    <a class="nav-link" href="{{ route( 'song' ) }}" title="Sånger">Sånger</a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'dokument' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'document' ) }}">Dokument</a>
+                    <a class="nav-link" href="{{ route( 'document' ) }}" title="Dokument">Dokument</a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'kontakt' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'contact' ) }}">Kontakta oss</a>
+                    <a class="nav-link" href="{{ route( 'contact' ) }}" title="Kontakta oss">Kontakta oss</a>
                 </li>
                 <li class="nav-item">
-                    <a rel="noreferrer" target="_blank" class="nav-link" href="https://tenta.lindacarlstad.se/linda">Tentor</a>
+                    <a rel="noreferrer" target="_blank" class="nav-link" href="https://tenta.lindacarlstad.se/linda" title="Tentor">Tentor</a>
                 </li>
             </ul>
         </div>
@@ -47,7 +47,7 @@
 
 <nav class="navbar navbar-dark bg-dark fixed-bottom scrolling-navbar d-block d-md-none border-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url( '/' ) }}">
+        <a class="navbar-brand" href="{{ url( '/' ) }}" title="Start">
             <img class="" src="{{ asset( 'img/small-logo.png') }}" alt="Golden apple with crown">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -56,32 +56,32 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="nav navbar-nav">
                 <li class="nav-item {{ Request::is('/') ? 'active' : null }}">
-                    <a class="nav-link" href="{{ url( '/' ) }}">Start<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ url( '/' ) }}" title="Start">Start<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'program' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'programs' ) }}">Program</a>
+                    <a class="nav-link" href="{{ route( 'programs' ) }}" title="Program">Program</a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'föreningen' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'about' ) }}">Föreningen</a>
+                    <a class="nav-link" href="{{ route( 'about' ) }}" title="Föreningen">Föreningen</a>
                 </li>
                 @if( session()->has( 'initiation' ) )
                     <li class="nav-item {{ Request::segment(1) === 'nollning' && Request::segment(2) === session()->get( 'initiation' ) ? 'active' : null }}">
-                        <a class="nav-link" href="{{ route( 'initiation.show', session()->get( 'initiation' ) ) }}">Nollning {{ session()->get( 'initiation' ) }}</a>
+                        <a class="nav-link" href="{{ route( 'initiation.show', session()->get( 'initiation' ) ) }}" title="Nollning {{ session()->get( 'initiation ) }}">Nollning {{ session()->get( 'initiation' ) }}</a>
                     </li>
                 @endif
             </ul>
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item {{ Request::segment(1) === 'sånger' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'song' ) }}">Sånger</a>
+                    <a class="nav-link" href="{{ route( 'song' ) }}" title=""Sånger>Sånger</a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'dokument' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'document' ) }}">Dokument</a>
+                    <a class="nav-link" href="{{ route( 'document' ) }}" title="Dokument">Dokument</a>
                 </li>
                 <li class="nav-item {{ Request::segment(1) === 'kontakt' ? 'active' : null }}">
-                    <a class="nav-link" href="{{ route( 'contact' ) }}">Kontakta oss</a>
+                    <a class="nav-link" href="{{ route( 'contact' ) }}" title="Kontakta oss">Kontakta oss</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="https://tenta.lindacarlstad.se/linda">Tentor</a>
+                    <a class="nav-link" href="https://tenta.lindacarlstad.se/linda" title="Tentor">Tentor</a>
                 </li>
             </ul>
         </div>
