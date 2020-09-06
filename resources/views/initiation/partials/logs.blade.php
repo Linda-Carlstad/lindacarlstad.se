9f8d7e2dd009909febb2dad5b4d7001146599c34
Felix Wetell <felix.wetell@gmail.com>
2020-09-06 21:18:44 +0200

    Fixes #135, log messages working as intended

--- resources/views/initiation/partials/logs.blade.php
+++ resources/views/initiation/partials/logs.blade.php
@@ -1,33 +1,31 @@
-<div class="d-block row d-md-flex py-4 px-2">
-    @foreach( $logs as $log )
-        <div class="col-12 mx-2 alert alert-danger alert-dismissible fade show" role="alert">
-            <b>{{ ucfirst( __( 'status.' . $log->action ) ) }} ({{ Carbon\Carbon::parse( $log->date )->diffForHumans() }})</b>
-            @foreach( json_decode( $log->data, true ) as $when => $attributes )
-                @if( $when === 'after' )
+@foreach( $logs as $log )
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <b>{{ ucfirst( __( 'status.' . $log->action ) ) }} ({{ Carbon\Carbon::parse( $log->date )->diffForHumans() }})</b>
+        @foreach( json_decode( $log->data, true ) as $when => $attributes )
+            @if( $when === 'after' )
+                @php
+                    $i = 0;
+                    $length = count( $attributes )
+                @endphp
+                <br>
+                @foreach( $attributes as $attribute => $value )
+                    {{ ucfirst( __( 'initiation.' . $attribute ) ) }}:
+                    @if( $attribute === 'initiation_id' )
+                        {{ App\Initiation::find( $value )->first()->year }}
+                    @else
+                        {{ strlen( $value ) > 120 ? substr( $value, 0, strpos( $value,' ', 30 ) ) . '...' : $value }}
+                    @endif
+                    @if( $i != $length - 1 )
+                        <br>
+                    @endif
                     @php
-                        $i = 0;
-                        $length = count( $attributes )
+                        $i++;
                     @endphp
-                    <br>
-                    @foreach( $attributes as $attribute => $value )
-                        {{ ucfirst( __( 'initiation.' . $attribute ) ) }}:
-                        @if( $attribute === 'initiation_id' )
-                            {{ App\Initiation::find( $value )->first()->year }}
-                        @else
-                            {{ strlen( $value ) > 120 ? substr( $value, 0, strpos( $value,' ', 30 ) ) . '...' : $value }}
-                        @endif
-                        @if( $i != $length - 1 )
-                            <br>
-                        @endif
-                        @php
-                            $i++;
-                        @endphp
-                    @endforeach
-                @endif
-            @endforeach
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-    @endforeach
-</div>
+                @endforeach
+            @endif
+        @endforeach
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endforeach
