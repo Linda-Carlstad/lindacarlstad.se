3ca9a0b3259b531b4dbc6883fe52c04d62b26f11
Felix Wetell <felix.wetell@gmail.com>
2021-01-08 05:31:09 +0100

    Only displays initiations that are active

--- app/Http/Controllers/InitiationController.php
+++ app/Http/Controllers/InitiationController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
 
 class InitiationController extends Controller
@@ -18,11 +19,11 @@ class InitiationController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|Response|View
      */
     public function index()
     {
-        $initiations = Initiation::all()->sortByDesc( 'year' );
+        $initiations = Initiation::where( 'active', true )->get()->sortByDesc( 'year' );
 
         return view( 'initiation.index' )->with( 'initiations', $initiations );
     }
@@ -30,7 +31,7 @@ class InitiationController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -40,8 +41,8 @@ class InitiationController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -77,7 +78,7 @@ class InitiationController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -87,9 +88,9 @@ class InitiationController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -100,7 +101,7 @@ class InitiationController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
