d4ff8efa3458b0dc5ee18b916e69aef61f2326cb
Felix Wetell <felix.wetell@gmail.com>
2021-03-12 02:55:27 +0100

    Adds npm packages and remove links #114

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -57,47 +57,9 @@
 <link href="{{ asset( '/img/icon.ico' ) }}" rel="icon">
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha256-NXRS8qVcmZ3dOv3LziwznUHPegFhPZ1F/4inU7uC8h0=" crossorigin="anonymous"></script>
-<script src="{{ asset( '/js/main.js' ) }}" type="text/javascript"></script>
-<script src="{{ asset( '/js/error.js' ) }}" type="module"></script>
-<script src="https://cdn.jsdelivr.net/ga-lite/latest/ga-lite.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+<script src="{{ asset( '/js/app.js' ) }}" type="text/javascript"></script>
 <script src="https://www.google.com/recaptcha/api.js?render={{ env( 'GOOGLE_RECAPTCHA_KEY' ) }}"></script>
-<script>
-    var galite = galite || {};
-    galite.UA = 'UA-136489552-1';
-    window.addEventListener("load", function(){
-        window.cookieconsent.initialise({
-            "palette": {
-                "popup": {
-                    "background": "#343a40"
-                },
-                "button": {
-                    "background": "#660023",
-                    "color": "#fff"
-                }
-            },
-            "content": {
-                "message": "Den här webbplatsen använder cookies för att du ska få den bästa upplevelsen på vår hemsida.",
-                "dismiss": "Uppfattat!",
-                "link": "Läs mer"
-            }
-        })});
 
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1618121,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
 <script defer type="application/ld+json">
   {
     "@context": "http://schema.org",
