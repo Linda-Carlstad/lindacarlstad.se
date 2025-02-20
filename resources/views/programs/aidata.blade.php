b3ab6678610440d2a0af8058baff44044fd9b55d
SpwaN <kevin.berling@hotmail.com>
2025-02-20 17:22:02 +0100

    Create aidata.blade.php
    Skapat kurssida för den nya utbildningen "Artificiell intelligens - Kandidatprogram i datavetenskap".

---
+++ resources/views/programs/aidata.blade.php
@@ -0,0 +1,32 @@
+@section( 'title', 'Artificiell intelligens - Kandidatprogram i datavetenskap' )
+@extends( 'layouts.app' )
+@section( 'content' )
+
+    <h1>Artificiell intelligens - Kandidatprogram i datavetenskap</h1>
+    <hr>
+    <p>
+        Brinner du för teknikens framtid och vill du vara en del av AI-revolutionen? Detta Bachelor 
+      of Science-program i datavetenskap med fokus på artificiell intelligens (AI) och maskininlärning (ML) 
+      förbereder dig för att ligga i framkant när det gäller morgondagens digitala framsteg. Genom att överbrygga 
+      klyftan mellan etablerad expertis inom datavetenskap och banbrytande AI-tekniker utrustar programmet dig med 
+      färdigheter för att driva innovation både etiskt och effektivt inom industri och forskning.
+      Genom en kombination av teoretiska kurser och praktisk erfarenhet, särskilt under det tredje året, 
+      får du en omfattande förståelse för AI-drivna lösningar. Näringslivet integreras i utbildningen genom 
+      gästföreläsningar och praktikmöjligheter, vilket säkerställer att du är beredd att tillämpa dina kunskaper 
+      i verkliga scenarier. Som utexaminerad student kommer du att vara redo att bidra till det föränderliga landskapet 
+      av AI-teknik, med självförtroende, expertis och det ansvar som krävs för att forma framtiden.
+    </p>
+    <p><b>Utbildningsnivå:</b> Grundutbildning</p>
+    <h3>Behöringhetskrav</h3>
+    <p>
+        Grundläggande behörighet. Matematik 3c/D
+    </p>
+    <p><b>Vill du läsa mer om utbildningen? Klicka på knappen nedan.</b></p>
+    <a rel="noreferrer" target="_blank" class="btn btn-grey" href="https://www.kau.se/utbildning/program-och-kurser/program/TGKAI">Läs mer</a>
+    <hr>
+    <div class="text-center">
+        <p>Klicka nedan för att gå tillbaks till alla program.</p>
+        <a class="btn btn-grey" href="{{ route( 'programs' ) }}">Program</a>
+    </div>
+    
+@endsection
