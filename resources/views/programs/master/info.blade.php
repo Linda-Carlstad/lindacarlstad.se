95b90cc8ec87417cac5603e607dd8ab4d1e3bc22
Jonathan Lång <96290813+jonathanlaang@users.noreply.github.com>
2025-02-22 23:29:47 +0100

    Updated program descriptions (#181)
    Updated the program descriptions to the most recent. Code cleanup, updated hyperlinks.

--- resources/views/programs/master/info.blade.php
+++ resources/views/programs/master/info.blade.php
@@ -7,14 +7,14 @@
     <p>
         Masterprogrammet i informatik/informationssystem är ett program på avancerad nivå för dig som
         vill fördjupa dina kunskaper om hur människor utvecklar och använder e-tjänster för att stödja
-        olika verksamheter i företag och samhälle.Det finns möjlighet att studera till magisterexmen (1 år)
+        olika verksamheter i företag och samhälle. Det finns möjlighet att studera till magisterexmen (1 år)
          inom masterprogrammets ram.
     </p>
     <p><b>Utbildningsnivå:</b> Avancerad nivå</p>
     <h3>Behöringhetskrav</h3>
     <p>
         Gymnasiets Engelska kurs 6 eller B eller motsvarande, kandidatexamen 180 hp varav minst 90 hp inom
-        Informatik eller Datavetenskap.
+        informatik eller datavetenskap.
     </p>
     <p><b>Vill du läsa mer om utbildningen? Klicka på knappen nedan.</b></p>
     <a rel="noreferrer" target="_blank" class="btn btn-grey" href="https://www.kau.se/utbildning/program-och-kurser/program/SAINF">Läs mer</a>
